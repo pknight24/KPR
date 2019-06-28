7ef2c7a4997e3ff5d833281a90d8cd52ae2e048f
Knight <pknight@fredhutch.org>
2019-06-28 14:39:15 -0700

    Scales flax

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' Baseline microbiome data (X) from a study on the effects of flaxseed lignan extract intervention on enterolactone production (Y).
 #' The H matrix is a similarity kernel generated from unweighted Unifrac distances between the samples. The Q matrix is a similarity kernel generated from an Aitchisen variation matrix computed between the p variables.
-#' The Y vector is already centered, and the X matrix has already been normalized with the centered log ratio and then column-centered.
+#' The Y vector is already centered and scaled, and the X matrix has already been normalized with the centered log ratio and then column-centered and scaled.
 #' @usage data(flax)
 #' @format A list of the four matrices descibed above.
 #' @references Lampe et al. (2019) The American Journal of Clinical Nutrition
