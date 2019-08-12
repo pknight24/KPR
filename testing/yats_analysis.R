3b1bb3f6b8e97b99fe37aaf9dc22d54101b66c61
Knight <pknight@fredhutch.org>
2019-08-12 16:45:47 -0700

    Test adaptive Q

--- testing/yats_analysis.R
+++ testing/yats_analysis.R
@@ -13,16 +13,27 @@ geo <- yatsunenko$geography
 
 rm(yatsunenko)
 
+n <- nrow(counts)
+p <- ncol(counts)
+
 counts.clr <- t(apply(log(counts + 1), 1, function(x) x - mean(x)))
 Z <- apply(counts.clr, 2, function(x) x - mean(x))
 Y <- log(age) - mean(log(age)) # centered log age
+E <- model.matrix(~ geo)[,-1]
 
-Q <- generateSimilarityKernel(patristic)
+alpha <- 0.01
+
+Q <- (1 - alpha) * generateSimilarityKernel(patristic) + alpha * diag(p)
+
+# K.ec <- solve(ec %*% t(ec)) %>%
+#   (function(x) x / svd(x)$d[1])
+
+model.fit <- KPR(designMatrix = Z, Y = Y, Q = Q)
 
 
-model.fit.Q <- KPR(designMatrix = Z, Y = Y, Q = Q)
-model.fit <- KPR(designMatrix = Z, Y = Y)
+plot(model.fit$beta.hat, col = ifelse(model.fit$p.values < 0.05,
+                                        "blue", "grey"),
+     ylab="Effect size", main = "", xlab = "",
+     pch = 19)
+abline(a = 0, b = 0, col = "red")
 
-plot(model.fit.Q$beta.hat, col = ifelse(model.fit.Q$p.values < 0.05,
-                                        "blue", "black"))
-abline(a = 0, b = 0)
