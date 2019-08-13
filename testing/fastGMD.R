87cbba3906735fa9a5368a3bbe0877b159c83475
Knight <pknight@fredhutch.org>
2019-08-13 16:03:26 -0700

    update fastgmd test

--- testing/fastGMD.R
+++ testing/fastGMD.R
@@ -12,3 +12,27 @@ fastGMD <- function(X, H, Q)
   d.star <- svd.X$d
   return(list(U = U.star, V = V.star, D = d.star))
 }
+
+library(KPR)
+
+data("yatsunenko")
+
+counts <- yatsunenko$raw.counts
+age <- yatsunenko$age
+unifrac <- yatsunenko$unifrac
+patristic <- yatsunenko$patristic
+ec <- yatsunenko$ec
+geo <- yatsunenko$geography
+
+rm(yatsunenko)
+
+n <- nrow(counts)
+p <- ncol(counts)
+
+counts.clr <- t(apply(log(counts + 1), 1, function(x) x - mean(x)))
+Z <- apply(counts.clr, 2, function(x) x - mean(x))
+Y <- log(age) - mean(log(age)) # centered log age
+
+Q <- generateSimilarityKernel(patristic)
+gmd <- GMD(X = Z, H = diag(n), Q = Q, K = length(svd(Z)$d))
+fgmd <- fastGMD(Z, diag(n), Q)
