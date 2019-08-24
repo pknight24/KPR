5be6e791c61fd1f6557530708527f5145a64e4b1
Parker Knight <pknight24@protonmail.com>
2019-08-24 10:37:08 -0400

    Remove option for cross validation

--- R/KPR.R
+++ R/KPR.R
@@ -7,11 +7,7 @@
 #' @param Y An n x 1 response vector. Should be scaled and centered.
 #' @param H An n x n sample similarity kernel. Must be symmetric and positive semidefinite. This defaults to an identity matrix.
 #' @param Q A p x p variable similarity kernel. Must be symmetric and postive semidefinite. This defaults to an identity matrix.
-#' @param REML logical, indicates whether to use REML to find the optimal value of \code{lambda}. If a value of \code{lambda} is given, this is ignored.
-#' @param n.lambda The number of lambda values to test through the cross validation search. The values are generated internally.
 #' @param lambda A vector of lambda values to test through cross validation. This will override the sequence generated with the \code{n.lambda} parameter.
-#' @param K The number of folds in the cross validation search.
-#' @param useCpp Indicate whether to use the C++ backend for cross-validation. This is ignored if `REML` is set to TRUE.
 #' @param seed Set a seed for random number generation.
 #' @param scale Logical, indicates whether to scale \code{Q} and the design matrix.
 #' @param inference Logical, indicates whether to compute p-values for penalized regression coefficients with the GMD inference.
@@ -20,13 +16,6 @@
 #' \item{beta.hat}{A matrix of estimated coefficients for the penalized variables, where each column corresponds to a different value of lambda.}
 #' \item{eta.hat}{A matrix of estimated coefficients for the unpenalized variables, where each column corresponds to a different value of lambda.}
 #' \item{lambda}{The vector of lambda values used in cross validation.}
-#' \item{lambda.min}{The value of lambda that results in the minimum sum of squared errors.}
-#' \item{lambda.min.index}{The index of \code{lambda.min} in the \code{lambda} vector. This can be used to retrieve the estimated coefficients corresponding
-#' to \code{lambda.min} from the \code{betahat} matrix.}
-#' \item{lambda.1se}{The largest value of lambda within one standard error of \code{lambda.min}.}
-#' \item{lambda.1se.index}{The index of the \code{lambda.1se} value in the \code{lambda} vector.}
-#' \item{cv.errors}{The error matrix generated in the cross-validation procedure.}
-#' \item{REML}{Was REML used to find \code{lambda}?}
 #' \item{p.values}{P-values for each penalized coefficient, resulting from the GMD inference.}
 #' \item{bound}{The stochastic bound used to compute each p-value.}
 #' @importFrom stats sd pnorm
@@ -41,7 +30,7 @@
 #' @useDynLib KPR, .registration = TRUE
 #' @export
 KPR <- function(designMatrix, covariates, Y, H = diag(nrow(designMatrix)), Q = diag(ncol(designMatrix)),
-                REML = TRUE, n.lambda = 200, lambda, K = 5, useCpp = TRUE, seed, scale = TRUE,
+                lambda, seed, scale = TRUE,
                 inference = TRUE, ...)
 {
   if (scale)
@@ -64,53 +53,8 @@ KPR <- function(designMatrix, covariates, Y, H = diag(nrow(designMatrix)), Q = d
   if (cov.missing) E <- matrix(0, n) # arbitrarily set E to a zero vector if no covariates are given
   else E <- as.matrix(covariates)
 
-  if (REML & missing(lambda)) # we don't need to use REML if lambda is given.
-  {
-    lambda <- remlEstimation(Z, E, Y, H, Q, cov.missing)
-    lambda.min <- NULL
-    lambda.min.index <- NULL
-    lambda.1se <- NULL
-    lambda.1se.index <- NULL
-    errors <- NULL
-
-  }
-  else
-  {
-    REML <- FALSE
-    if(missing(lambda))
-      lambda <- exp(seq(from = 0, to = 10, length.out = n.lambda))
-    n.lambda <- length(lambda)
-
-    if (n.lambda == 1)
-    {
-      lambda.min <- NULL
-      lambda.min.index <- NULL
-      lambda.1se <- NULL
-      lambda.1se.index <- NULL
-      errors <- NULL
-    }
-    else
-    {
-      if (!missing(seed)) set.seed(seed)
-      randidx <- sample(1:n, n)
-      Yrand <- Y[randidx]
-      Zrand <- Z[randidx, ]
-      Erand <- as.matrix(E[randidx, ])
-      Hrand <- H[randidx, randidx]
-      if (useCpp) errors <- computeErrorMatrixCpp(Zrand, Erand, Yrand, Hrand, Q, lambda, K, cov.missing)
-      else errors <- computeErrorMatrixR(Zrand, Erand, Yrand, Hrand, Q, lambda, K, cov.missing)
-      lambda.min.index <- which.min(colSums(errors))
-      lambda.min <- lambda[lambda.min.index]
-
-      se.QH.KPR <- sd(errors[, lambda.min.index]) * sqrt(K)
-      lambda.1se.indices <- which(colSums(errors) < sum(errors[,lambda.min.index]) + se.QH.KPR) # this gets all of the indices of the lambda values withing one standard error of the min
-
-      lambda.1se <- max(lambda[lambda.1se.indices])
-      lambda.1se.index <- which(lambda == lambda.1se)
-    }
-
-  }
-
+  lambda <- remlEstimation(Z, E, Y, H, Q, cov.missing)
+  
   estimates <- sapply(lambda, FUN = function(s) {
       if (cov.missing) P <- diag(n)
       else P <- diag(n) - E %*% solve(t(E) %*% H %*% E) %*% t(E) %*% H
@@ -139,13 +83,7 @@ KPR <- function(designMatrix, covariates, Y, H = diag(nrow(designMatrix)), Q = d
               Q = Q,
               beta.hat = beta.hat,
               eta.hat = eta.hat,
-              lambda = lambda,
-              lambda.min = lambda.min,
-              lambda.min.index = lambda.min.index,
-              lambda.1se = lambda.1se,
-              lambda.1se.index = lambda.1se.index,
-              cv.errors = errors,
-              REML = REML)
+              lambda = lambda)
   if (inference) {
     infer.out <- GMD.inference(output,...)
     output$p.values <- infer.out$p.values
