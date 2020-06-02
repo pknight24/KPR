38093ac45fbc411e7ecf9a52cdd72a35ee6ee4ad
Parker Knight <pknight24@protonmail.com>
2020-06-02 11:48:54 -0400

    Update methods

--- R/methods.R
+++ R/methods.R
@@ -8,20 +8,6 @@ print.KPR <- function(x, ...)
   q.trivial <- ifelse(all(x$Q == diag(ncol(x$Z))), "trivial","nontrivial" )
   h.trivial <- ifelse(all(x$H == diag(nrow(x$Z))), "trivial","nontrivial" )
   cat("with a", h.trivial, "H matrix and a", q.trivial, "Q matrix.\n\n")
-  if (x$REML)
-  {
-    cat("Lambda found with REML estimation: ", x$lambda, "\n")
-  }
-  else
-  {
-    if (length(x$lambda) == 1) cat("Given value of lambda: ", x$lambda, "\n")
-    else
-    {
-        cat("Lambda values evaluated with", nrow(x$cv.errors), "fold cross validation.\n")
-        cat("    lambda.min:", x$lambda.min, "at index",x$lambda.min.index,"of", length(x$lambda),"\n")
-        cat("    lambda.1se:", x$lambda.1se,"at index",x$lambda.1se.index,"of", length(x$lambda),"\n")
-    }
-  }
 
 }
 
@@ -33,23 +19,10 @@ summary.KPR <- function(object, ...)
 
   infer.out <- object$p.vals
 
-  if (length(object$lambda) == 1)
-  {
-    b <- object$beta.hat
-    l <- c(round(object$lambda), rep("", length(b) - 1))
-    p <- infer.out
-
-    data.frame(lambda = l, betahat = b, pvalue = p)
-
-  }
-  else
-  {
-    b <- object$beta.hat[,object$lambda.min.index]
-    l <- c(round(object$lambda.min), rep("", length(b) - 1))
-    p <- infer.out[,object$lambda.min.index]
-
-    data.frame(lambda = l, betahat = b, pvalue = p)
-  }
+  b <- object$beta.hat
+  l <- c(round(object$lambda), rep("", length(b) - 1))
+  p <- infer.out
+  data.frame(lambda = l, betahat = b, pvalue = p)
 
 }
 
