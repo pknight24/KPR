ad538b7e726bf0967ce750de0c56599eee5d5268
Parker Knight <plb.knight@gmail.com>
2021-02-10 18:05:03 -0500

    Begin Rcpp for AD

---
+++ R/reml.R
@@ -0,0 +1,21 @@
+remlEstimation <- function(Z.p, Y.p, H, Q)
+{
+
+  n <- nrow(Z.p)
+  p <- ncol(Z.p)
+
+    L.H <- t(chol(H))
+  L.Q <- t(chol(Q))
+  Z.tilde <- t(L.H) %*% Z.p %*% L.Q
+  Y.tilde <- t(L.H) %*% Y.p
+
+  u <- svd(Z.tilde)$u
+  s <- svd(Z.tilde)$d
+
+  dummyID <- factor(rep(1, n))
+  lmm.fit <- lme(Y.tilde~1, random=list(dummyID = pdIdent(~-1 + u %*% diag(s))))
+
+  lambda.reml <- lmm.fit$sigma^2 / as.numeric(VarCorr(lmm.fit)[1,1])
+
+  return(lambda.reml)
+}
--- src/code.cpp
+++ src/code.cpp
@@ -1,2 +1,38 @@
+// [[Rcpp::depends(RcppEigen)]]
+// [[Rcpp::depends(StanHeaders)]]
+#include <stan/math.hpp>
 #include <Rcpp.h>
 #include <RcppEigen.h>
+
+using namespace Rcpp;
+using namespace Eigen;
+
+/**
+This function should compute the gradient of the KPR likelihood at vector theta using AD.
+**/
+
+ // [Rcpp::export]
+ VectorXd gradient_auto(VectorXd theta_) // this needs to also take arguments corresponding to X, Y, H, and Q
+ {
+
+   std::vector<stan::math::var> theta(theta_.size()); // create a new vector of type stan::math::var and copy the contents of theta_ to it
+   for (int i = 0; i < theta.size(); i++)
+     { theta[i] = theta_[i];}
+
+   VectorXd grad(theta_.size()); // value of the gradient of likelihood at theta
+
+   // now we need to compute the likelihood using all the given data, save it as type stan::math::var, and then call the .grad() method to compute the gradient
+
+
+   // a quick test:
+   // this compiles without issue
+   stan::math::var x = theta[0];
+   stan::math::var y = x + 10;
+   y.grad();
+
+
+
+
+
+   return(grad);
+ }
